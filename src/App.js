e7508b6746d047dcd8a9eadab6576ca0019ac813
MAliRagipCakir <m-ar-cakir@hotmail.com>
2022-03-17 12:59:03 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState("")
+  const [todos, setTodos] = useState([
+    {
+      title: "Apply for a job",
+      isDone: false
+    },
+    {
+      title: "Wash dishes",
+      isDone: false
+    },
+    {
+      title: "Do Laundry",
+      isDone: true
+    }
+  ]);
+
+  const handleSubmit = function (event) {
+    event.preventDefault();
+    const newTodos = [...todos, { title: title, isDone: false }];
+    newTodos.sort((a, b) => a.isDone - b.isDone)
+    setTodos(newTodos);
+    setTitle("");
+  }
+
+  const updateItem =function (event, index) {
+    const newTodos = [...todos];
+    newTodos[index].isDone = event.target.checked;
+    newTodos.sort((a, b) => a.isDone - b.isDone)
+    setTodos(newTodos);
+  };
+
+  const deleteItem =function (event, index) {
+    event.preventDefault();
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todos</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" className="New-todo" placeholder="Do something.." value={title} onChange={(e) => setTitle(e.target.value)} required />
+      </form>
+      <ul className="App-todos">
+        {todos.map((x, i) =>
+          <li key={i} className={"Todo-item " + (x.isDone ? "Done" : "Undone")}>
+            <input type="checkbox" className="Todo-check" checked={x.isDone} onChange={(e) => updateItem(e, i)} />
+            <span className="Todo-title">{x.title}</span>
+            <a href="#" className="Todo-delete" onClick={(e) => deleteItem(e, i)}>&times;</a>
+          </li>
+        )}
+      </ul>
     </div>
   );
 }
